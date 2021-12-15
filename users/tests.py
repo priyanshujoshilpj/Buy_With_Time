622e802424a779ec5de4490afda518aa00c1fac2
priyanshujoshilpj <joshipriyanshu85@gmail.com>
2021-12-15 23:11:53 +0530

    Devops- Unit Test(Url Tests)

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,30 @@
+from django.http import response
 from django.test import TestCase
 
 # Create your tests here.
+
+class URLTests(TestCase):
+    def test_testhomepage(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_testsignup(self):
+        response = self.client.get('/signup')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_testsignin(self):
+        response = self.client.get('/signin')
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_testaboutus(self):
+        response = self.client.get('/aboutus')
+        self.assertEqual(response.status_code, 200)
+
+    def test_testcontactus(self):
+        response = self.client.get('/contactus')
+        self.assertEqual(response.status_code, 200)
+
+    def test_testsignout(self):
+        response = self.client.get('/signout')
+        self.assertEqual(response.status_code, 302)
